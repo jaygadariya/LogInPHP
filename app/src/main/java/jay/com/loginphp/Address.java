211893dae5e2f8074d428f7f5e062d26dffb5dc4
Jay Gadariya <jaygadariya7397@gmail.com>
2018-07-02 11:17:15 +0530

    jay

---
+++ app/src/main/java/jay/com/loginphp/Address.java
@@ -0,0 +1,30 @@
+package jay.com.loginphp;
+
+public class Address {
+    String Locality;
+    String AdminArea;
+    String CountryName;
+    String PostalCode;
+    String FeatureName;
+
+
+    public String getLocality() {
+        return Locality;
+    }
+
+    public String getAdminArea() {
+        return AdminArea;
+    }
+
+    public String getCountryName() {
+        return CountryName;
+    }
+
+    public String getPostalCode() {
+        return PostalCode;
+    }
+
+    public String getFeatureName() {
+        return FeatureName;
+    }
+}
